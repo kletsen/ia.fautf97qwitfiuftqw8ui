7eec8e858a336002a12bb93f3036f969a4b953f4
kletsen <114153830+kletsen@users.noreply.github.com>
2024-06-11 12:03:13 -0300

    ok

--- script.js
+++ script.js
@@ -50,7 +50,16 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 
-mostraPergunta()
+mostraPergunta();
+
 
