a470966749be29afe700e50aa9e9d04ef93a9c21
kletsen <114153830+kletsen@users.noreply.github.com>
2024-06-11 12:01:57 -0300

    ok

--- script.js
+++ script.js
@@ -44,3 +44,13 @@ const perguntas = [
     }
 ]
 
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta()
+
